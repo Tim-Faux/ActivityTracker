efdd8aef3735a8611c0d08173586b13460bc2e38
Tim-Faux <tim-faux@comcast.net>
2025-06-10 08:37:29 -0400

    Added Activity suggestions section and updated font

--- ActivityTracker/Helpers/WordDocumentFormater.cs
+++ ActivityTracker/Helpers/WordDocumentFormater.cs
@@ -21,6 +21,9 @@ namespace ActivityTracker.Helpers
 			paragraph.BreakCharacterFormat.FontSize = 12f;
 
 			CreateTable(section, daysInWeek);
+			IWSection activitySuggestionSection = document.AddSection();
+			AddActivitySuggestionDaysOfTheWeek(activitySuggestionSection);
+
 			return document;
 		}
 
@@ -42,30 +45,35 @@ namespace ActivityTracker.Helpers
 			IWTextRange textRange = paragraph.AppendText("Monday");
 			mondayCell.Width = columnWidth;
 			textRange.CharacterFormat.Bold = true;
+			textRange.CharacterFormat.FontName = "Aptos";
 
 			var tuesdayCell = table.Rows[0].Cells[1];
 			paragraph = tuesdayCell.AddParagraph();
 			textRange = paragraph.AppendText("Tuesday");
 			tuesdayCell.Width = columnWidth;
 			textRange.CharacterFormat.Bold = true;
+			textRange.CharacterFormat.FontName = "Aptos";
 
 			var wednesdayCell = table.Rows[0].Cells[2];
 			paragraph = wednesdayCell.AddParagraph();
 			textRange = paragraph.AppendText("Wednesday");
 			wednesdayCell.Width = columnWidth;
 			textRange.CharacterFormat.Bold = true;
+			textRange.CharacterFormat.FontName = "Aptos";
 
 			var thursdayCell = table.Rows[0].Cells[3];
 			paragraph = thursdayCell.AddParagraph();
 			textRange = paragraph.AppendText("Thurday");
 			thursdayCell.Width = columnWidth;
 			textRange.CharacterFormat.Bold = true;
+			textRange.CharacterFormat.FontName = "Aptos";
 
 			var fridayCell = table.Rows[0].Cells[4];
 			paragraph = fridayCell.AddParagraph();
 			textRange = paragraph.AppendText("Friday");
 			fridayCell.Width = columnWidth;
 			textRange.CharacterFormat.Bold = true;
+			textRange.CharacterFormat.FontName = "Aptos";
 
 			return table;
 		}
@@ -81,12 +89,15 @@ namespace ActivityTracker.Helpers
 
 					IWTextRange textRange = paragraph.AppendText($"{singleDayInformation.Activity} ");
 					textRange.CharacterFormat.TextColor = Color.Red;
+					textRange.CharacterFormat.FontName = "Aptos";
 
 					textRange = paragraph.AppendText($"{singleDayInformation.StaffNames}\r");
 					textRange.CharacterFormat.TextColor = Color.Blue;
+					textRange.CharacterFormat.FontName = "Aptos";
 
 					textRange = paragraph.AppendText(singleDayInformation.ClientNames);
 					textRange.CharacterFormat.TextColor = Color.Black;
+					textRange.CharacterFormat.FontName = "Aptos";
 					currentCell.Width = columnWidth;
 
 					table.Rows[currentRow].Height = contentCellHeight;
@@ -96,6 +107,19 @@ namespace ActivityTracker.Helpers
 			}
 
 			return table;
+		}
+
+		private static void AddActivitySuggestionDaysOfTheWeek(IWSection section)
+		{
+			var paragraph = section.AddParagraph();
+			paragraph.ParagraphFormat.AfterSpacing = 8f;
+			IWTextRange daysOfTheWeek = paragraph.AppendText("Week schedule\r" +
+				"Monday: \r" +
+				"Tuesday: \r" +
+				"Wednesday: \r" +
+				"Thursday: \r" +
+				"Friday: \r");
+			daysOfTheWeek.CharacterFormat.FontName = "Aptos";
 		}
 	}
 }
