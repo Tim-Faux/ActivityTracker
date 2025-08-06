9b66ab06d3273df3a5bfed1c4a8bb68068639317
Tim-Faux <tim-faux@comcast.net>
2025-08-06 14:56:02 -0400

    Added ability to enter Syncfusion license

--- ActivityTracker/Helpers/SyncfusionLicenseRegister.cs
+++ ActivityTracker/Helpers/SyncfusionLicenseRegister.cs
@@ -22,11 +22,30 @@ namespace ActivityTracker.Helpers
 			var noteFile = (StorageFile) await storageFolder.TryGetItemAsync(fileName);
 			if (noteFile is not null) {
 				var license = await FileIO.ReadTextAsync(noteFile);
-				SyncfusionLicenseProvider.RegisterLicense(license);
+				SyncfusionLicenseProvider.RegisterLicense(license.Trim());
 			}
 			else {
 				await storageFolder.CreateFileAsync(fileName);
 			}
+		}
+
+		public static async Task SaveSyncfusionLicense(string license)
+		{
+			StorageFolder storageFolder;
+			try {
+				storageFolder = await ApplicationData.Current.LocalFolder.GetFolderAsync(InputFilesFolder);
+			}
+			catch {
+				storageFolder = await ApplicationData.Current.LocalFolder.CreateFolderAsync(InputFilesFolder);
+			}
+
+			var noteFile = (StorageFile) await storageFolder.TryGetItemAsync(fileName);
+			if (noteFile is null) {
+				noteFile = await storageFolder.CreateFileAsync(fileName);	
+			}
+
+			SyncfusionLicenseProvider.RegisterLicense(license.Trim());
+			await FileIO.WriteTextAsync(noteFile, license);
 		}
 	}
 }
--- ActivityTracker/MainWindow.xaml.cs
+++ ActivityTracker/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Threading.Tasks;
 using ActivityTracker.Helpers;
 using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
 using Syncfusion.Licensing;
 
 namespace ActivityTracker
@@ -8,17 +10,70 @@ namespace ActivityTracker
     public sealed partial class MainWindow : Window
     {
         public MainWindow()
-        {
+		{
 			this.InitializeComponent();
 
 			ExtendsContentIntoTitleBar = true;
 			SetTitleBar(AppTitleBar);
 
 			Task.Run(() => SyncfusionLicenseRegister.RegisterSyncfusionLicense()).Wait();
-			bool isValid = SyncfusionLicenseProvider.ValidateLicense(Platform.WinUI);
+			var isValid = SyncfusionLicenseProvider.ValidateLicense(Platform.WinUI);
 			if (!isValid) {
 				SyncfusionLicenseError.Visibility = Visibility.Visible;
 			}
+		}
+
+		private async Task ValidateSyncfusionLicense()
+		{
+			var isValid = false;
+			var numberOfAttempts = 0;
+			do {
+				isValid = SyncfusionLicenseProvider.ValidateLicense(Platform.WinUI);
+				if (!isValid) {
+					SyncfusionLicenseError.Visibility = Visibility.Visible;
+					await Task.Delay(500);
+				}
+				else {
+					SyncfusionLicenseError.Visibility = Visibility.Collapsed;
+				}
+			} while (!isValid && numberOfAttempts < 10);
+		}
+
+		private async void SyncfusionLicenseLink_Click(Microsoft.UI.Xaml.Documents.Hyperlink sender, Microsoft.UI.Xaml.Documents.HyperlinkClickEventArgs args)
+		{
+			var text = new TextBlock
+			{
+				Text = "Enter your Syncfusion license and hit enter:",
+				Margin = new Thickness(10)
+			};
+			var syncfusionLicenceTextbox = new TextBox
+			{
+				PlaceholderText = "Enter your license",
+				Width = 1000,
+				Margin = new Thickness(10),
+				AcceptsReturn = false
+			};
+
+			var content = new StackPanel();
+			content.Children.Insert(0, text);
+			content.Children.Insert(1, syncfusionLicenceTextbox);
+
+			ContentDialog LicenseErrorDialog = new ContentDialog
+			{
+				XamlRoot = this.Content.XamlRoot,
+				Title = "Syncfusion License Error",
+				Content = content,
+				PrimaryButtonText = "Enter",
+				CloseButtonText = "Cancel",
+				DefaultButton = ContentDialogButton.Primary
+			};
+
+			ContentDialogResult result = await LicenseErrorDialog.ShowAsync();
+			if (result == ContentDialogResult.Primary) {
+				await SyncfusionLicenseRegister.SaveSyncfusionLicense(syncfusionLicenceTextbox.Text.Trim());
+			}
+
+			await ValidateSyncfusionLicense();
 		}
 	}
 }
